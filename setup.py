91025f5374c834c3d51f13014258fa5c793a4b25
Fantix King <fantix@uchicago.edu>
2019-02-18 20:34:02 -0600

    chore(pypi): auto release notes and PyPI

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
 from setuptools import setup, find_packages
+from subprocess import check_output
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 setup(
-    name='datamodelutils',
-    version='0.0.0',
+    name="datamodelutils",
+    version=get_version(),
     packages=find_packages(),
+    install_requires=[
+        "cdisutils",
+        "psqlgraph~=1.2",
+        "dictionaryutils>=1.2.0",
+        "gdcdictionary~=1.1",
+        "gdcdatamodel~=1.1",
+    ],
     entry_points={
-        'console_scripts': [
-            'datamodel_postgres_admin=datamodelutils.postgres_admin:main',
-            'datamodel_repl=datamodelutils.repl:main'
+        "console_scripts": [
+            "datamodel_postgres_admin=datamodelutils.postgres_admin:main",
+            "datamodel_repl=datamodelutils.repl:main",
         ]
     },
 )

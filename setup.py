7d90084e0bf73a299fa758742fd21a7d3b76899c
Zoe Chitty <19900057+vpsx@users.noreply.github.com>
2019-06-20 11:27:49 -0500

    bump psqlgraph, sqlalchemy

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     packages=find_packages(),
     install_requires=[
         "cdisutils",
-        "psqlgraph~=1.2",
+        "psqlgraph~=2.0",
         "dictionaryutils>=1.2.0",
         "gen3dictionary~=2.0",
         "gen3datamodel~=2.0",
